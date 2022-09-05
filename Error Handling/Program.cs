11ba5a12d3e244a351685f0c2f329643956afe0c
Pius Ashogbon <Pius1ash@gmail.com>
2022-09-05 14:17:49 +0100

    Add project files.

---
+++ Error Handling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Error_Handling
+{
+    class Program
+    {
+
+        int result;
+
+        Program()
+        {
+            result = 0;
+        }
+
+        public void Division(int[] a)
+        {
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                { 
+                    int z = i + 1;
+                    result = Math.Max(result, a[i] / a[z]);
+                }
+
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+
+            finally
+            {
+                Console.WriteLine($"The result is {result}");
+            }
+        }
+        static void Main(string[] args)
+        {
+           var program = new Program();
+            int[] a = { 24, 5, 0, 56};
+            program.Division(a);
+            
+        }
+    }
+}
